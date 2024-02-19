b12578c4fc817d484d0bc276e00a124e969b8773
Nathan Clark <tokenshift@gmail.com>
2024-02-19 11:09:43 -0500

    Cleanup

--- src/views/PageGalleryTileImage.svelte
+++ src/views/PageGalleryTileImage.svelte
@@ -10,15 +10,31 @@
   export let page: Page
   export let view: ViewConfig
 
-  async function getDisplayInfo (page: Page, view: ViewConfig) {
+  type DisplayInfo = {
+    mode: 'content',
+    content: HTMLElement
+  } | {
+    mode: 'image',
+    imageSrc: string
+  } | {
+    mode: 'fallback'
+  }
+
+  async function getDisplayInfo (page: Page, view: ViewConfig): Promise<DisplayInfo> {
     const content = await pageContentService.getRenderedContentWithImages(page)
 
     if (view.mode === 'content') {
       // In 'content' mode, display page contents or a fallback (if the page is
       // empty), and don't bother looking for an image.
-      return {
-        mode: content ? 'content' : 'fallback',
-        content
+      if (content) {
+        return {
+          mode: 'content',
+          content
+        }
+      } else {
+        return {
+          mode: 'fallback'
+        }
       }
     } else {
       const imageSrc = await pageContentService.getFirstImageSrc(page)
@@ -29,8 +45,9 @@
           imageSrc
         }
       } else {
-        if (view.mode === 'image') {
+        if (view.mode === 'image' || !content) {
           // In 'image' mode, display a fallback if there is no image.
+          // If there's no content either, then display a fallback even in 'auto' mode.
           return {
             mode: 'fallback'
           }
@@ -38,7 +55,7 @@
           // In 'auto' mode, display page content if there is no image, or a
           // fallback if there isn't any content either.
           return {
-            mode: content ? 'content' : 'fallback',
+            mode: 'content',
             content
           }
         }
@@ -49,25 +66,25 @@
 
 {#await getDisplayInfo(page, view)}
   <div class="page-gallery__tile-image--loading">Loading...</div>
-{:then { mode, content, imageSrc }}
-  {#if mode === 'content'}
+{:then info}
+  {#if info.mode === 'content'}
     <a class="page-gallery__tile-image--content internal-link"
       data-href={page.file.path}
       href={page.file.path}
       rel="noopener">
       <div class="page-gallery__tile-image--content-sizer"></div>
-      <div class="page-gallery__tile-image--content-wrapper">{@html content.innerHTML}</div>
+      <div class="page-gallery__tile-image--content-wrapper">{@html info.content.innerHTML}</div>
     </a>
-  {:else if mode === 'image'}
+  {:else if info.mode === 'image'}
     <a class="page-gallery__tile-image--image internal-link"
       data-href={page.file.path}
-      data-image-src={imageSrc}
+      data-image-src={info.imageSrc}
       href={page.file.path}
       rel="noopener"
-      style:background-image={`url('${imageSrc}')`}>
+      style:background-image={`url('${info.imageSrc}')`}>
       <span class="page-gallery__hidden">Link to {page.file.name}</span>
     </a>
-  {:else if mode === 'fallback'}
+  {:else if info.mode === 'fallback'}
     <a class="page-gallery__tile-image--fallback internal-link"
       data-href={page.file.path}
       href={page.file.path}
